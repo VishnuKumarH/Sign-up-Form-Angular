8ed47950a67beb4a144c83be8635dff4256524db
VishnuKumarH <130031369+VishnuKumarH@users.noreply.github.com>
2023-11-30 16:16:51 +0530

    Add files via upload

---
+++ Sign_up Angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { SignupComponent } from './signup/signup.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    JumbotronComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
